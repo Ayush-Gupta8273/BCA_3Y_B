e034dedcaf1e2db59577c4ed4cdbf09c62320463
Ayush-Gupta8273 <ayush.ag8273@gmail.com>
2024-08-20 14:56:11 +0530

    Portfolio Project

---
+++ Portfolio/script.js
@@ -0,0 +1,16 @@
+let student={
+    id: 101,
+    sname: "Ayush",
+    age: 21,
+    isWorking: false,
+    img:""
+}
+let{id,sname,age,isWorking,imag}=student
+let info = `
+    <h1>Welcome</h1>
+    <img src="${img}" alt="No image" height="100px" width="100px"><br>
+    <h2>ID: ${id}</h2>
+    <h3>Age: ${age} yr old </h3>
+    <h3>Working/Student: ${isWorking ? "Working profession": "Student"}</h3>
+`
+document.write(info)
---
+++ script.js
@@ -0,0 +1,5 @@
+function calculate(){
+    let num1= parsefloat(document.getElementById("num1").value);
+    let num2= parsefloat(document.getElementById("num2").value);
+    let num3= parsefloat(document.getElementById("num2").value);
+}
